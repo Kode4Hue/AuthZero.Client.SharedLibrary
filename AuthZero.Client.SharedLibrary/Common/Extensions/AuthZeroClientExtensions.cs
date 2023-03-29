7b19d060604e03c7847a73bc62c84ccf852368f5
kodehue <hubertgraham@outlook.com>
2023-03-29 18:56:52 -0500

    added services extension registration for authzero

---
+++ AuthZero.Client.SharedLibrary/Common/Extensions/AuthZeroClientExtensions.cs
@@ -0,0 +1,23 @@
+﻿using AuthZero.Client.SharedLibrary.Common.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthZero.Client.SharedLibrary.Common.Extensions
+{
+    public static class AuthZeroClientExtensions
+    {
+
+        public static IServiceCollection AddAuthZeroClient(this IServiceCollection services, IConfiguration configuration, string configSectionName)
+        {
+            AuthZeroConfiguration config = new();
+            services.Configure<AuthZeroConfiguration>(t =>
+            {
+                configuration.GetSection(configSectionName).Bind(t);
+                config = t;
+            });
+
+            services.AddHttpClient<AuthZeroClient>();
+            return services;
+        }
+    }
+}
