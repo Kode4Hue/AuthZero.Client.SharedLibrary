26856d5aa42fe38dc685dc0da5b905e8249de1ea
Hubert Graham <hubert.graham90@gmail.com>
2023-04-10 16:29:06 -0500

    added audience property for configuration file

--- AuthZero.Client.SharedLibrary/Common/Configuration/AuthZeroConfiguration.cs
+++ AuthZero.Client.SharedLibrary/Common/Configuration/AuthZeroConfiguration.cs
@@ -6,6 +6,7 @@
         public string BaseAddress { get; set; }
         public string ApiVersion { get; set; }
         public string ApiPath { get; set; }
+        public string Audience { get; set; }
         public AuthZeroClient Client { get; set; }
         public string Connection { get; set; }
         public bool ShouldLogHttpRequest { get; set; }
