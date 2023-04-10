d0faa128446599125f8e88c7a066c15b212dea35
Hubert Graham <hubert.graham90@gmail.com>
2023-04-10 16:06:33 -0500

    added grant type constants

---
+++ AuthZero.Client.SharedLibrary/Common/Constants/GrantTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthZero.Client.SharedLibrary.Common.Constants
+{
+    internal static class GrantTypes
+    {
+        public const string Password = "password";
+        public const string ClientCredentials = "client_credentials";
+    }
+}
