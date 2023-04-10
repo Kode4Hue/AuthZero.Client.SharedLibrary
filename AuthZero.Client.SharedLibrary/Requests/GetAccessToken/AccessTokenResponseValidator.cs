5770f07c2d7dc56a4178d4e1aa10cbd2b8e58dba
Hubert Graham <hubert.graham90@gmail.com>
2023-04-10 15:49:49 -0500

    added validator for access token response

---
+++ AuthZero.Client.SharedLibrary/Requests/GetAccessToken/AccessTokenResponseValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace AuthZero.Client.SharedLibrary.Requests.GetAccessToken
+{
+    public class AccessTokenResponseValidator: AbstractValidator<AccessTokenResponse>
+    {
+        public AccessTokenResponseValidator()
+        {
+            RuleFor(x => x.AccessToken)
+                .NotEmpty();
+            RuleFor(x => x.ExpiresIn)
+                .GreaterThan(0);
+        }
+    }
+}
